3a489c7132168eb1b0ccb3727d3648a5cece7788
PikaPool2402 <tigerkushgupta@gmail.com>
2024-06-24 19:27:53 +0530

    initial commit

---
+++ 01. Node Tutorial/01-intro.js
@@ -0,0 +1,10 @@
+const amount = 9;
+
+if (amount < 10) {
+    console.log("small number!");
+} else {
+    console.log("large number!");
+}
+
+// can use template strings also!
+console.log(`hey, it's my first node app!`);
