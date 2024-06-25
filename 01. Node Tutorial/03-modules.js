51c0e72b9c1b8af4e7bc240ead6d6bb38a1ee965
PikaPool2402 <tigerkushgupta@gmail.com>
2024-06-25 14:40:47 +0530

    added names and utils

---
+++ 01. Node Tutorial/03-modules.js
@@ -0,0 +1,17 @@
+// const peter = "peter";
+// const john = "john";
+
+// const sayHi = (name) => {
+//     console.log(`hello there ${name}`);
+// };
+// sayHi("susan");
+// sayHi(peter);
+// sayHi(john);
+
+// We use CommonJS library inside node applications
+// In CommonJS, every file in node is a module (by default)
+// Modules - Encapsulated Code (only share minimum principle)
+
+sayHi("susan");
+sayHi(peter);
+sayHi(john);
