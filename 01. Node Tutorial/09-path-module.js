f3e97dd249d5c1e723ea5c14fae9ed1f16afba8f
PikaPool2402 <tigerkushgupta@gmail.com>
2024-06-26 13:14:01 +0530

    finished path module

--- 01. Node Tutorial/09-path-module.js
+++ 01. Node Tutorial/09-path-module.js
@@ -5,18 +5,18 @@ const path = require("path");
 console.log(path.sep);
 
 // join path-segments using above seperator!
-// returns normalized path, autocorrect slashes!
+// returns normalized path, autocorrect trailing slashes!
 const filePath = path.join(
-    "/test-folder/////",
+    "test-folder/////",
     "test-subfolder",
     "test-file.txt"
 );
 console.log(filePath);
 
-// return the last-portion of the path!
+// return the last-portion (base) of the path!
 const base = path.basename(filePath);
 console.log(base);
 
 // return an absolute path!
-const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
+const absolute = path.resolve("test-folder", "test-subfolder", "test-file.txt");
 console.log(absolute);
