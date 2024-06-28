6ad1736858ba4ad44b24903e41bde50d49ee35e3
PikaPool2402 <tigerkushgupta@gmail.com>
2024-06-28 12:34:00 +0530

    edited server.js

--- 01. Node Tutorial/01-event-loop-examples/04-server.js
+++ 01. Node Tutorial/01-event-loop-examples/04-server.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 // everytime a request comes in, we execute below callback!
 const server = http.createServer((req, res) => {
+    // this cb is not executed immediately!
     console.log("request event");
     res.end("hello world");
 });
@@ -10,5 +11,5 @@ server.listen(5000, () => {
     console.log("server listening on port : 5000...");
 });
 // listen - asynchronous function!
-// stays alive, constantly listening for requests!
+// stays alive, constantly listening for requests (request-events!
 // event loop waits for requests to come in, and then executes the cb!
